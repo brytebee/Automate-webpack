c109cf572e920f5e7ee0817c491c09d647e80746
Atsighi Bright <brytebee@gmail.com>
2021-11-09 13:47:50 +0100

    Update index.html & index.js files

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import './style.css';
 const root = document.querySelector('#root');
 const h1 = document.createElement('h1');
 h1.textContent = 'Hello World from index.js';
-root.append(h1);
+root.append(h3);
